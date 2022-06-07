c4fbd4379eb4f38ddad8a10bc3dd9f406874fddc
Aditya Nur Huda <56750600+Noisycools@users.noreply.github.com>
2022-06-07 20:45:48 +0700

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  content: ["index.html"],
+  theme: {
+    extend: {
+      dropShadow: {
+        '2lg': '0 10px 8px rgb(0 0 0 / 0.35)',
+      },
+      maxHeight: {
+        '552': '552px',
+      },
+      hueRotate: {
+        '160': '-160deg',
+        160: '160deg',
+        '20': '-20deg',
+        20: '20deg',
+      }
+    },
+  },
+  plugins: [],
+};
